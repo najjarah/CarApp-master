f388c74a906673a7bad81b61ef35c60ee388705c
ahmed <ahmed@LAPTOP-DTIN7NTV>
2019-01-08 20:51:52 +0200

    ..

--- src/main/java/com/example/carApp/CarAppApplication.java
+++ src/main/java/com/example/carApp/CarAppApplication.java
@@ -1,6 +1,6 @@
 package com.example.carApp;
 
-      import java.util.List;
+//      import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
